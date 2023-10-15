e0d29084562769c43a1c3c7d61afda37e51c0789
venomcodes0943 <dhcoes0943@gmail.com>
2023-10-15 10:38:45 +0500

    Search Categories part ends

---
+++ script.js
@@ -0,0 +1,16 @@
+function showCategorie() {
+    const show = document.querySelector('.categorie-option');
+    show.classList.toggle('categorie-option-show')
+}
+window.onclick = function (event) {
+    if (!event.target.matches('.select-icon')) {
+        var dropdowns = document.getElementsByClassName("categorie-option");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('categorie-option-show')) {
+                openDropdown.classList.remove('categorie-option-show');
+            }
+        }
+    }
+}
