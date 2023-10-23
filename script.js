dce22aec3cf65fe7b6c7ec0fcfd2d60127508cda
venomcodes0943 <dhcoes0943@gmail.com>
2023-10-23 22:36:32 +0500

    adding responsive sidebar

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function showCategorie() {
     const show = document.querySelector('.categorie-option');
     show.classList.toggle('categorie-option-show');
 }
+
+function openNavSide() {
+    if (window.screen.width <= 1129 && window.screen.width >= 768) {
+        document.querySelector(".dts-sidebar-side").style.width = "44%";
+    }
+    else if (window.screen.width <= 760) {
+        document.querySelector(".dts-sidebar-side").style.width = "96%";
+    }
+}
+function hideNavSide() {
+    document.querySelector(".dts-sidebar-side").style.width = "0%";
+}
+
 window.onclick = function (event) {
     if (!event.target.matches('.select-icon')) {
         var dropdowns = document.getElementsByClassName("categorie-option");
